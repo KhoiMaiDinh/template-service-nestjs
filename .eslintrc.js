f68ef3857d2455785f692cfa71822219bcc7680c
kaifeng101 <kaifeng.ng.2022@scis.smu.edu.sg>
2024-08-19 09:42:06 +0700

    chore: update eslint config: enforce type safety, stricter object types, and prettier formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,46 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': [
+    'error',
+      {
+        types: {
+          'Object': {
+            message: 'Avoid using the `Object` type. Did you mean `object`?',
+          },
+          'Function': {
+            message: 'Avoid using the `Function` type. Prefer a specific function type, like `() => void`.',
+          },
+          'Boolean': {
+            message: 'Avoid using the `Boolean` type. Did you mean `boolean`?',
+            fixWith: 'boolean',
+          },
+          'Number': {
+            message: 'Avoid using the `Number` type. Did you mean `number`?',
+            fixWith: 'number',
+          },
+          'Symbol': {
+            message: 'Avoid using the `Symbol` type. Did you mean `symbol`?',
+            fixWith: 'symbol',
+          },
+          'String': {
+            message: 'Avoid using the `String` type. Did you mean `string`?',
+            fixWith: 'string',
+          },
+          '{}': {
+            message: 'Use Record<K, V> instead',
+            fixWith: 'Record<K, V>',
+          },
+          'object': {
+            message: 'Use Record<K, V> instead',
+            fixWith: 'Record<K, V>',
+          },
+        },
+      },
+    ],
+    'prettier/prettier': [
+      'error', {singleQuote: true, trailingComma: 'all', tabWidth: 2, bracketSpacing: true},
+      "error", { "endOfLine": "auto" }
+    ],
   },
 };
